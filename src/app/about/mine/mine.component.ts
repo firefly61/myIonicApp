fefe209ea2c4ecc0215a657fbd1b3d872a95f682
codingYing <nicoledeyx@163.com>
2018-11-22 19:11:04 +0800

    animate

--- src/app/about/mine/mine.component.ts
+++ src/app/about/mine/mine.component.ts
@@ -1,13 +1,26 @@
 import {Component, OnInit} from '@angular/core';
 import {ModalController} from '@ionic/angular';
 import {MinePage} from './mine.page';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
     selector: 'app-mine',
     templateUrl: './mine.component.html',
-    styleUrls: ['./mine.component.scss']
+    styleUrls: ['./mine.component.scss'],
+    animations: [trigger('circle', [
+        state('start', style({'opacity': 1})),
+        state('end', style({'opacity': 0})),
+        transition('start => end', [
+            animate('1s')
+        ]),
+        transition('end => start', [
+            animate('0.5s')
+        ])
+    ])]
 })
 export class MineComponent implements OnInit {
+
+    isOpen = true;
 
     constructor(
         private modalCtrl: ModalController
@@ -15,6 +28,10 @@ export class MineComponent implements OnInit {
     }
 
     ngOnInit() {
+    }
+
+    toggle() {
+        this.isOpen = !this.isOpen;
     }
 
     async openModal() {
